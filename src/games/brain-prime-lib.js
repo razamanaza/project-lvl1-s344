9642a5b8b1cecb697c4bbc371ec16cfecce54064
razamanaza <razamanaza@yandex.ru>
2018-09-15 13:41:27 +1000

    Add brain-progression game

---
+++ src/games/brain-prime-lib.js
@@ -0,0 +1,23 @@
+import * as index from '..';
+import { cons } from 'hexlet-pairs';
+
+const isPrime = (num) => {
+  if (num <= 1) return false;
+  const iter = (startNum, count) => {
+    if (count <= 1) return true;
+    if (startNum % count === 0) return false;
+    return iter(startNum, count - 1);
+  };
+  return iter(num, Math.floor(Math.sqrt(num)));
+};
+
+const getQuestionAnswer = () => {
+  const question = index.getRandom(2, 1000);
+  const answer = ((isPrime(question)) ? 'yes' : 'no');
+  return cons(question.toString(), answer);
+};
+
+export default () => {
+  const greeting = 'Find the greatest common divisor of given numbers.';
+  index.startGame(getQuestionAnswer, greeting);
+};
